ae2395261b82bb620880a13c1c226e59c58e6a80
servan-unal <71839049+servan-unal@users.noreply.github.com>
2023-05-02 13:57:44 +0300

    Completed Task 2: Writed validation that checks every posted field

--- classes/ConstructionStagesPatchValidator.php
+++ classes/ConstructionStagesPatchValidator.php
@@ -8,6 +8,34 @@ class ConstructionStagesPatchValidator {
      * @return void
      */
     public static function validate(ConstructionStagesPatch $patch) {
+        if (strlen($patch->name) > 255) {
+            throw new Exception("Name cannot be longer than 255 characters");
+        }
+
+        if (!empty($patch->start_date) && !DateTime::createFromFormat(DateTime::ISO8601, $patch->start_date)) {
+            throw new Exception("Start date must be a valid date&time in iso8601 format i.e. 2022-12-31T14:59:00Z");
+        }
+
+        if (!empty($patch->end_date)) {
+            if (!DateTime::createFromFormat(DateTime::ISO8601, $patch->end_date)) {
+                throw new Exception("End date must be a valid datetime in iso8601 format i.e. 2022-12-31T14:59:00Z");
+            }
+
+            $start = new DateTime($patch->start_date);
+            $end = new DateTime($patch->end_date);
+            if ($end <= $start) {
+                throw new Exception("End date must be later than the start date");
+            }
+        }
+
+        if (!empty($patch->color) && !preg_match('/^#[a-f0-9]{6}$/i', $patch->color)) {
+            throw new Exception("Color must be a valid HEX color i.e. #FF0000");
+        }
+
+        if (!empty($patch->externalId) && strlen($patch->externalId) > 255) {
+            throw new Exception("External ID cannot be longer than 255 characters");
+        }
+
         $validStatuses = ["NEW", "PLANNED", "DELETED"];
         if (!empty($patch->status) && !in_array($patch->status, $validStatuses)) {
             throw new Exception("Status must be one of NEW, PLANNED or DELETED");
